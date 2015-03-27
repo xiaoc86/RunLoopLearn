6f3152ee6fc407fa4867020a0aa256b8831c4a44
xiaoc86 <xiaoc86@gmail.com>
2015-03-27 15:13:55 +0800

    DEV:intpu str

--- RunLoopLearn1/RunLoopLearn1/ViewController.m
+++ RunLoopLearn1/RunLoopLearn1/ViewController.m
@@ -10,8 +10,16 @@
 #import "RunLoopThread.h"
 #import "CFRunLoopThread.h"
 #import "ObserverThread.h"
+#import "RLInputSourceThread.h"
+#import "RTInputSourceManager.h"
 
 @interface ViewController ()
+
+@property (nonatomic,assign) int                index;
+
+@property (nonatomic,strong) UIButton           * addbutton;
+@property (nonatomic,strong) UIButton           * firebutton;
+@property (nonatomic,strong) UIButton           * delbutton;
 
 @end
 
@@ -20,9 +28,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //[self runLoopThreadStart];
-    //[self cfRunLoopThreadStart];
-    [self observerThreadStart];
+    _index = 0;
+    
+    _addbutton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _addbutton.frame = CGRectMake(100, 100, 100, 100);
+    [_addbutton setTitle:@"添加子线程" forState:UIControlStateNormal];
+    [_addbutton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [self.view addSubview:_addbutton];
+    [_addbutton addTarget:self action:@selector(add) forControlEvents:UIControlEventTouchUpInside];
+    
+    _firebutton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _firebutton.frame = CGRectMake(100, 200, 100, 100);
+    [_firebutton setTitle:@"触发子线程" forState:UIControlStateNormal];
+    [_firebutton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [self.view addSubview:_firebutton];
+    [_firebutton addTarget:self action:@selector(fire) forControlEvents:UIControlEventTouchUpInside];
+    
+    _delbutton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _delbutton.frame = CGRectMake(100, 300, 100, 100);
+    [_delbutton setTitle:@"删除子线程" forState:UIControlStateNormal];
+    [_delbutton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [self.view addSubview:_delbutton];
+    [_delbutton addTarget:self action:@selector(del) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void) add
+{
+    [[RTInputSourceManager shareInstance] inputSourceThreadStart];
+}
+
+- (void) fire
+{
+    _index ++;
+    [[RTInputSourceManager shareInstance] fireInput:[NSString stringWithFormat:@"测试%d",_index]];
+}
+
+- (void) del
+{
+     //[[RTInputSourceManager shareInstance] fireInput:@"测试"];
 }
 
 - (void) runLoopThreadStart
@@ -42,6 +85,5 @@
     ObserverThread * ot = [[ObserverThread alloc] init];
     [ot start];
 }
-
 
 @end
